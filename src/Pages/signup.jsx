496a745a28a70cdd8c664af9e150a7e6c48f7a0d
Its-Anth0ny <bhattshubhankar27@gmail.com>
2024-01-16 17:02:16 +0530

    final commit

--- src/Pages/signup.jsx
+++ src/Pages/signup.jsx
@@ -21,7 +21,7 @@ const Signup = () => {
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
-            const response = await fetch('http://localhost:3000/api/users/signup', {
+            const response = await fetch('https://hero-backend-knh4.onrender.com/api/users/signup', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
